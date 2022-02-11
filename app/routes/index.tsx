d30e84cf913285d4de23d956367fae147630232d
Davi de Carvalho <davi_carvalho96@hotmail.com>
2022-02-11 20:02:10 -0300

    feat: arrumado redirecionamento de cadastrar

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -279,9 +279,9 @@ export default function Index() {
         <div className="max-w-5xl mx-auto">
           <div className="flex flex-col items-center md:flex-row">
             <div className="w-full space-y-5 md:w-3/5 md:pr-16">
-              <p className="font-medium text-blue-500 uppercase">
-                Building Businesses
-              </p>
+              {/*<p className="font-medium text-blue-500 uppercase">*/}
+              {/*  Building Businesses*/}
+              {/*</p>*/}
               <h2 className="text-2xl font-extrabold leading-none text-black sm:text-3xl md:text-5xl">
                 Mudando o jeito dos professores de gerirem suas aulas.
               </h2>
