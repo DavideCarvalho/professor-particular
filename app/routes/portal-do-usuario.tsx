ee95f569bad0c3fa2f9b4945a1621710c53f7c7f
Davi de Carvalho <davi_carvalho96@hotmail.com>
2022-02-18 21:02:07 -0300

    feat: portal do usuário apenas para usuários autenticados

--- app/routes/portal-do-usuario.tsx
+++ app/routes/portal-do-usuario.tsx
@@ -2,23 +2,26 @@ import { LoaderFunction, redirect } from 'remix';
 import Stripe from 'stripe';
 import qs from 'qs';
 import { supabase } from '~/lib/supabase/supabase.server';
+import {getUserByRequestToken, isAuthenticated} from "~/lib/auth";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: '2020-08-27',
 });
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const [url, queryString] = request.url.split('?');
-  const { userId } = qs.parse(queryString) as {
-    userId: string;
-  };
+  if (!(await isAuthenticated(request))) return redirect('/login');
+  const { user } = await getUserByRequestToken(request);
+  const [url] = request.url.split('?');
   const { data, error } = await supabase
     .from('user')
     .select('stripe_id')
-    .match({ id: userId })
+    .match({ id: user.id })
     .single();
   if (!data || error) {
     throw new Error('User not found');
+  }
+  if (!data.stripe_id) {
+    throw new Error(`User doesn't have a payment method yet`);
   }
   const [, , urlWithoutSufix] = url.split('/');
   const session = await stripe.billingPortal.sessions.create({
