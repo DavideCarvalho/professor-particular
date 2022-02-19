7d818cfa238ecbd9b272b852c61f38a1109232bf
Davi de Carvalho <davi_carvalho96@hotmail.com>
2022-02-19 10:17:21 -0300

    feat: limitando criação de sala por pagamento

--- app/routes/salas/index.tsx
+++ app/routes/salas/index.tsx
@@ -1,8 +1,12 @@
-import {LoaderFunction, redirect, useLoaderData, Form, Link} from 'remix';
+import { LoaderFunction, redirect, useLoaderData, Form, Link } from 'remix';
 import { isAuthenticated, getUserByRequestToken } from '~/lib/auth';
 import { AppLayout } from '~/components/AppLayout';
 import { Card } from '~/components/Card';
 import { supabase } from '~/lib/supabase/supabase.server';
+import { stripeClient } from '~/lib/stripe/stripe.server';
+import type { Stripe } from 'stripe';
+import { FC, useState } from 'react';
+import { ModalCannotCreateClassroom } from '~/components/modal-cannot-create-classroom';
 
 type StudentsAttrs = {
   className: string;
@@ -19,14 +23,30 @@ export let loader: LoaderFunction = async ({ request }) => {
     .select(
       `
       id,
+      stripe_id,
       role(name)
     `
     )
     .match({ id: user.id })
     .single();
+
   if (!foundUser || foundUserError) {
     return redirect('/login');
   }
+
+  let userSubscription: Stripe.Subscription | undefined;
+  let userPlan: any;
+
+  if (foundUser?.stripe_id) {
+    const stripeUser = (await stripeClient.customers.retrieve(
+      foundUser.stripe_id,
+      { expand: ['subscriptions'] }
+    )) as Stripe.Customer;
+
+    userSubscription = stripeUser?.subscriptions?.data?.[0];
+    userPlan = (userSubscription as any).plan;
+  }
+
   const { role } = foundUser;
   const { data: students, error } = await supabase
     .from('classroom')
@@ -50,19 +70,50 @@ export let loader: LoaderFunction = async ({ request }) => {
       };
     }),
     user: foundUser,
+    userPlan,
     error,
   };
 };
 
+function isPaying(userPlanId: string): boolean {
+  return (
+    !userPlanId ||
+    userPlanId === 'price_1KTD3ZIgRgyZD761SGBTV0Xi' ||
+    userPlanId === 'price_1KTD3wIgRgyZD761Ky8hFRt2'
+  );
+}
+
+const ButtonNewClassroom: FC<any> = ({ userPlan, students }) => {
+  const [showModal, setShowModal] = useState(false);
+  if (!isPaying(userPlan?.id) && students.length >= 2) {
+    return (
+      <>
+        <ModalCannotCreateClassroom
+          isOpen={showModal}
+          close={() => setShowModal(false)}
+        />
+        <button onClick={() => setShowModal(true)} className="btn">
+          + sala
+        </button>
+      </>
+    );
+  }
+  return (
+    <Link to="nova-sala" className="btn">
+      + sala
+    </Link>
+  );
+};
+
 export default function ProfessorStudentsPage() {
-  const { students, user } =
-    useLoaderData<{ students: StudentsAttrs[]; user?: any }>();
+  const { students, user, userPlan } =
+    useLoaderData<{ students: StudentsAttrs[]; user?: any; userPlan: any }>();
   return (
     <AppLayout user={user}>
       <div className="flex flex-col justify-center items-center relative">
         <h1 className="text-5xl">Suas salas</h1>
         <div className="flex justify-end w-full">
-          <Link to="nova-sala" className="btn">+ sala</Link>
+          <ButtonNewClassroom userPlan={userPlan} students={students} />
         </div>
         <div className="py-8 grid grid-cols-1 w-full">
           <div>
